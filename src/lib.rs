a105734924c12a98b4bdd6a15c36c5fea3bec1f2
Speykious <speykious@gmail.com>
2023-10-23 15:51:46 +0200

    Add a `From<HashMap>` and `set_property` method

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,24 @@ impl<'a> fmt::Display for PropertyNotFoundError<'a> {
 #[derive(Debug, Clone, PartialEq, Eq, Default)]
 pub struct Properties(HashMap<String, String>);
 
+impl From<HashMap<String, String>> for Properties {
+    fn from(value: HashMap<String, String>) -> Self {
+        Self(value)
+    }
+}
+
 impl Properties {
+    /// Gets the corresponding value for a property key.
     pub fn get_property<'a>(&self, key: &'a str) -> Result<&'_ str, PropertyNotFoundError<'a>> {
         self.0
             .get(key)
             .map(String::as_ref)
             .ok_or(PropertyNotFoundError(key))
+    }
+
+    /// Sets a value for a property key.
+    pub fn set_property(&mut self, key: String, value: String) -> Option<String> {
+        self.0.insert(key, value)
     }
 
     pub fn write_without_spaces<W: Write>(&self, writer: &mut W) -> Result<(), io::Error> {
@@ -53,7 +65,7 @@ impl Properties {
                 padded
             };
 
-            writeln!(writer, "{padded_key} = {value}")?;
+            writeln!(writer, "{} = {}", padded_key, value)?;
         }
 
         Ok(())
